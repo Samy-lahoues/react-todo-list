13711c9c5692f894e0f9987ccf07dd9f98b61417
Samy Lahoues <samylahoues@outlook.com>
2025-07-20 22:58:58 +0100

    make the app responsive and compatible with mobile screens

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,40 +1,68 @@
+import { Home, Info, Languages } from "lucide-react";
+import useIsMobile from "../../hooks/useIsMobile";
 import { translations } from "../../constants";
 import { useLanguage } from "../../hooks/useLanguage";
 import ThemeToggler from "../ui/ThemeToggler";
 
 const Navbar = () => {
-    const { isArabic, toggleIsArabic } = useLanguage();
+  const { isArabic, toggleIsArabic } = useLanguage();
+  const isMobile = useIsMobile();
 
-    return (
-        <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-5xl px-4 md:px-6 py-2 bg-secondary-bg text-text-primary shadow-lg rounded-b-xl z-50">
-            <nav className="flex flex-col md:flex-row md:justify-between md:items-center space-y-2 md:space-y-0">
-                <div className="text-3xl font-bold tracking-tight">
-                    {translations.logo[isArabic ? "ar" : "en"]}
-                </div>
-                <div className="flex items-center gap-6">
-                    <a
-                        href="/"
-                        className="text-text-primary hover:text-btn-info transition-colors duration-300"
-                    >
-                        Home
-                    </a>
-                    <a
-                        href="/about"
-                        className="text-text-primary hover:text-btn-info transition-colors duration-300"
-                    >
-                        About
-                    </a>
-                    <button
-                        onClick={toggleIsArabic}
-                        className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-3 rounded-lg font-medium transition-colors flex items-center justify-center gap-2 cursor-pointer"
-                    >
-                        {translations.language[isArabic ? "en" : "ar"]}
-                    </button>
-                    <ThemeToggler />
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-5xl px-4 md:px-6 py-2 bg-secondary-bg text-text-primary shadow-lg rounded-b-xl z-50">
+      <nav className="flex flex-col md:flex-row md:justify-between items-center space-y-2 md:space-y-0">
+        {/* Logo */}
+        <a
+          href="/"
+          className="text-3xl font-extrabold tracking-tight hover:text-btn-info transition-colors"
+        >
+          {translations.logo[isArabic ? "ar" : "en"]}
+        </a>
+
+        {/* Navigation Items */}
+        <div className="flex items-center gap-6">
+          {/* Home Link */}
+          <a
+            href="/"
+            className="text-text-primary hover:text-btn-info transition-colors duration-300"
+          >
+            {isMobile ? (
+              <Home className="w-5 h-5 text-black dark:text-white" />
+            ) : (
+              "Home"
+            )}
+          </a>
+
+          {/* About Link */}
+          <a
+            href="/about"
+            className="text-text-primary hover:text-btn-info transition-colors duration-300"
+          >
+            {isMobile ? (
+              <Info className="w-5 h-5 text-black dark:text-white" />
+            ) : (
+              "About"
+            )}
+          </a>
+
+          {/* Language Toggle */}
+          <button
+            onClick={toggleIsArabic}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-3 rounded-lg font-medium transition-colors flex items-center justify-center gap-2"
+          >
+            {isMobile ? (
+              <Languages className="w-5 h-5 text-white" />
+            ) : (
+              translations.language[isArabic ? "en" : "ar"]
+            )}
+          </button>
+
+          {/* Theme Toggler */}
+          <ThemeToggler />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
--- src/components/ui/TodoCard.tsx
+++ src/components/ui/TodoCard.tsx
@@ -1,117 +1,126 @@
 import { Check, Calendar, Edit3, Trash2 } from "lucide-react";
 import { translations } from "../../constants";
 import type { ChangeEvent } from "react";
+import useIsMobile from "../../hooks/useIsMobile";
+
 type priorityTypes = "high" | "low" | "medium";
 
 const TodoCard = ({
-    text,
-    onComplete,
-    onPrioritize,
-    onDelete,
-    onEdit,
-    completed,
-    priority,
-    taskId,
-    isArabic,
-    dueDate,
+  text,
+  onComplete,
+  onPrioritize,
+  onDelete,
+  onEdit,
+  completed,
+  priority,
+  isArabic,
+  taskId,
+  dueDate,
 }: {
-    text: string;
-    onComplete: (taskId: number) => void;
-    onPrioritize: (taskId: number, priority: priorityTypes) => void;
-    completed: boolean;
-    priority: priorityTypes;
-    isArabic: boolean;
-    taskId: number;
-    dueDate: string;
-    onDelete: (taskId: number) => void;
-    onEdit: (taskId: number) => void;
+  text: string;
+  onComplete: (taskId: number) => void;
+  onPrioritize: (taskId: number, priority: priorityTypes) => void;
+  completed: boolean;
+  priority: priorityTypes;
+  isArabic: boolean;
+  taskId: number;
+  dueDate: string;
+  onDelete: (taskId: number) => void;
+  onEdit: (taskId: number) => void;
 }) => {
-    return (
-        <div
-            className={`bg-white dark:bg-gray-800 shadow-md p-6 transition-all hover:shadow-lg ${
-                completed && "opacity-70"
+  const isMobile = useIsMobile();
+
+  return (
+    <div
+      className={`bg-white dark:bg-gray-800 shadow-md p-3.5 md:p-6 transition-all hover:shadow-lg ${
+        completed && "opacity-70"
+      } rounded-lg`}
+    >
+      <div
+        className={`flex items-center justify-between ${
+          isMobile && "gap-4"
+        } overflow-hidden`}
+      >
+        {/* Left Side */}
+        <div className="w-fit flex gap-3.5 items-start">
+          <button
+            onClick={() => onComplete(taskId)}
+            className={`w-6 h-6 rounded-full border-2 flex items-center justify-center transition-colors ${
+              completed
+                ? "bg-emerald-500 border-emerald-500 text-white"
+                : "border-gray-300 dark:border-gray-600 hover:border-emerald-500"
             }`}
-        >
-            <div className="flex justify-between items-center overflow-hidden">
-                {/* left Side */}
-                <div className="w-fit flex gap-3.5 items-center">
-                    <button
-                        onClick={() => onComplete(taskId)}
-                        className={`w-6 h-6 rounded-full border-2 flex items-center justify-center transition-colors ${
-                            completed
-                                ? "bg-emerald-500 border-emerald-500 text-white"
-                                : "border-gray-300 dark:border-gray-600 hover:border-emerald-500"
-                        }`}
-                    >
-                        <Check className="w-4 h-4" />
-                    </button>
-                    <div className="flex flex-col gap-y-2.5">
-                        <p
-                            className={`text-lg font-medium ${
-                                completed && "line-through text-gray-500"
-                            }`}
-                        >
-                            {text}
-                        </p>
-                        {/* if todo completed : text-gray-800 */}
-                        <div
-                            className={`flex gap-2 ${
-                                isArabic && "flex-row-reverse"
-                            }`}
-                        >
-                            <select
-                                disabled={completed}
-                                onChange={(
-                                    event: ChangeEvent<HTMLSelectElement>
-                                ) =>
-                                    onPrioritize(
-                                        taskId,
-                                        event?.target.value as priorityTypes
-                                    )
-                                }
-                                value={priority}
-                                className={`text-sm capitalize font-semibold text-center px-1.5 py-1 rounded-xl hover:cursor-pointer ${
-                                    priority === "high"
-                                        ? "bg-red-600"
-                                        : priority === "medium"
-                                        ? "bg-yellow-600"
-                                        : "bg-green-600"
-                                }`}
-                            >
-                                <option value="low">low</option>
-                                <option value="medium">medium</option>
-                                <option value="high">high</option>
-                            </select>
-                            <div
-                                className={`text-sm text-gray-500 dark:text-gray-400 flex items-center gap-1`}
-                            >
-                                <Calendar className="w-4 h-4" />
-                                {/* due date */}
-                                <p>{dueDate}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* right Side */}
-                <div className=" flex gap-2.5">
-                    <button
-                        disabled={completed}
-                        onClick={() => onEdit(taskId)}
-                        className="bg-blue-500 hover:bg-blue-600 icon-button"
-                        title={translations.edit[isArabic ? "ar" : "en"]}
-                    >
-                        <Edit3 className="w-4 h-4" />
-                    </button>
-                    <button
-                        onClick={() => onDelete(taskId)}
-                        className="icon-button bg-red-500 hover:bg-red-600"
-                        title={translations.delete[isArabic ? "ar" : "en"]}
-                    >
-                        <Trash2 className="w-4 h-4" />
-                    </button>
-                </div>
+          >
+            <Check className="w-4 h-4" />
+          </button>
+          <div className="flex flex-col gap-y-2.5">
+            <p
+              className={`text-base md:text-lg font-medium ${
+                completed && "line-through text-gray-500"
+              }`}
+            >
+              {text}
+            </p>
+
+            <div
+              className={`flex flex-wrap gap-2 items-center ${
+                isArabic ? "flex-row-reverse" : ""
+              }`}
+            >
+              <select
+                disabled={completed}
+                onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+                  onPrioritize(taskId, event.target.value as priorityTypes)
+                }
+                value={priority}
+                className={`text-xs md:text-sm capitalize font-semibold text-center px-2 py-1 rounded-xl hover:cursor-pointer ${
+                  priority === "high"
+                    ? "bg-red-600"
+                    : priority === "medium"
+                      ? "bg-yellow-600"
+                      : "bg-green-600"
+                } text-white`}
+              >
+                <option value="low">low</option>
+                <option value="medium">medium</option>
+                <option value="high">high</option>
+              </select>
+
+              <div className="text-sm text-gray-500 dark:text-gray-400 flex items-center gap-1">
+                <Calendar className="w-4 h-4" />
+                <p className="text-xs md:text-sm">{dueDate}</p>
+              </div>
             </div>
+          </div>
         </div>
-    );
+
+        {/* Right Side */}
+        <div
+          className={`flex ${isMobile ? "justify-end gap-1.5" : "gap-2.5"} mt-2 md:mt-0`}
+        >
+          <button
+            disabled={completed}
+            onClick={() => onEdit(taskId)}
+            className={`bg-blue-500 hover:bg-blue-600 text-white transition-colors disabled:opacity-50 disabled:cursor-not-allowed rounded-md ${
+              isMobile ? "p-1.5" : "p-2"
+            }`}
+            title={translations.edit[isArabic ? "ar" : "en"]}
+          >
+            <Edit3 className="w-4 h-4" />
+          </button>
+          <button
+            onClick={() => onDelete(taskId)}
+            className={`bg-red-500 hover:bg-red-600 text-white transition-colors disabled:opacity-50 disabled:cursor-not-allowed rounded-md ${
+              isMobile ? "p-1.5" : "p-2"
+            }`}
+            title={translations.delete[isArabic ? "ar" : "en"]}
+          >
+            <Trash2 className="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default TodoCard;
